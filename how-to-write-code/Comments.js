d9a0003b34b45634ce4e6d543d9af04cbe66e421
Jack Van Patten <jackvanpatten@Autumns-Air-2.attlocal.net>
2023-08-20 10:56:56 -0500

    Experimented with new lines and tabs

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,6 +1,5 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
@@ -9,4 +8,4 @@
    multi-line
    comments. */
 
-   console.log("Comments make your code more readable by others.");
+   //console.log("Comments make your code more readable by others.");
