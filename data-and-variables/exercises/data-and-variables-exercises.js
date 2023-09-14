20a44d62158814debffb114be4182de57bf0885d
Jack Van Patten <jackvanpatten@Autumns-Air-2.attlocal.net>
2023-09-13 20:26:04 -0500

    Week 1 D&V assignment

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,24 @@
 // Declare and assign the variables below
-
+let nameOfTheShuttel = "Determination"
+let shuttelSpeedMph = 17500
+let distanceToMarsKm = 225000000
+let distanceToMoonKm = 384400
+let milesPerKm = 0.621
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof nameOfTheShuttel)
+console.log(typeof shuttelSpeedMph)
+console.log(typeof distanceToMarsKm)
+console.log(typeof distanceToMoonKm)
+console.log(typeof milesPerKm)
 // Calculate a space mission below
-
+let milesToMars = distanceToMarsKm * milesPerKm;
+let hoursToMars = milesToMars / shuttelSpeedMph;
+let daysToMars = hoursToMars / 24
 // Print the results of the space mission calculations below
-
+console.log( nameOfTheShuttel  +  "will take" +  daysToMars  +  "days to reach Mars.")
 // Calculate a trip to the moon below
-
+let milesToMoon = distanceToMoonKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttelSpeedMph;
+let daysToMoon = hoursToMoon / 24
 // Print the results of the trip to the moon below
+console.log(nameOfTheShuttel + "will take" + daysToMoon+ "days to reach the Moon")
--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,6 @@
 console.log('Hello, JavaScript.');
 console.log(2001);
-console.log("What","do","commas","do?");
+console.log("What","do","commas","do?")//; hi autumn//
 console.log("Does", "adding\t",      "space", "matter?");
-console.log('Launch' + 'Code\n');
-console.log("LaunchCode was founded in", 2013);
+console.log('Launch' + 'Code\n'); console.log("Hi!")
+console.log("LaunchCode was founded in", 2013)
